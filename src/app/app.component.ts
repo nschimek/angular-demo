34a7c4e85de007b794fb61b37178ab20e901b5ca
Nick Schimek <schimek@gmail.com>
2022-02-04 04:12:00 +0000

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ngrx-code-reuse-demo';
+
+  readonly test = 'hello world!';
+
+  ngOnInit(): void {
+    console.log("Hello world!");
+  }
 }
