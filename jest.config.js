bb5067ce51278c9d32533cafa67f40647c25f9bf
Nick Schimek <schimek@gmail.com>
2022-02-09 04:07:32 +0000

    installing jest

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+// require('jest-preset-angular/ngcc-jest-processor'); // necessary to force Jenkins to use AOT compiler
+
+const tsJestPreset = require('jest-preset-angular/jest-preset').globals['ts-jest'];
+
+module.exports = {
+  preset: "jest-preset-angular",
+  roots: ['src'],
+  setupFilesAfterEnv: ["<rootDir>/src/setup-jest.ts"],
+  verbose: true,
+  testURL: "http://localhost",
+  globals: {
+    'ts-jest': {
+      ...tsJestPreset,
+      tsconfig: "<rootDir>/tsconfig.spec.json"
+    }
+  }
+}
---
+++ src/setup-jest.ts
@@ -0,0 +1,5 @@
+import 'jest-preset-angular/setup-jest';
+import '@testing-library/jest-dom/extend-expect';
+import 'jest-when'
+
+Error.stackTraceLimit = 3;
